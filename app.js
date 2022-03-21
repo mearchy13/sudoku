055101b402727f3cc9aee29b71edd177972021f2
sn0569 <shyamnath@my.unt.edu>
2022-03-20 22:52:15 -0500

    Closes #5

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 count = 0
 for (let i = 0; i < 9; i++) {
     count = 9 * i;
-    document.getElementsByClassName("box")[i].innerHTML = "<div class='cell'><input type='text' id='" + (count + 1) +
-        "'class='input'></div><div class='cell'><input type='text' id='" + (count + 2) +
-        "'class='input'></div><div class='cell'><input type='text' id='" + (count + 3) +
-        "'class='input'></div><div class='cell'><input type='text' id='" + (count + 4) +
-        "'class='input'></div><div class='cell'><input type='text' id='" + (count + 5) +
-        "'class='input'></div><div class='cell'><input type='text' id='" + (count + 6) +
-        "'class='input'></div><div class='cell'><input type='text' id='" + (count + 7) +
-        "'class='input'></div><div class='cell'><input type='text' id='" + (count + 8) +
-        "'class='input'></div><div class='cell'><input type='text' id='" + (count + 9) + "'class='input'></div>"
+    document.getElementsByClassName("box")[i].innerHTML = "<div class='cell'><input type='text' maxlength='1' id='" + (count + 1) +
+        "'class='input'></div><div class='cell'><input type='text' maxlength='1' id='" + (count + 2) +
+        "'class='input'></div><div class='cell'><input type='text' maxlength='1' id='" + (count + 3) +
+        "'class='input'></div><div class='cell'><input type='text' maxlength='1' id='" + (count + 4) +
+        "'class='input'></div><div class='cell'><input type='text' maxlength='1' id='" + (count + 5) +
+        "'class='input'></div><div class='cell'><input type='text' maxlength='1' id='" + (count + 6) +
+        "'class='input'></div><div class='cell'><input type='text' maxlength='1' id='" + (count + 7) +
+        "'class='input'></div><div class='cell'><input type='text' maxlength='1' id='" + (count + 8) +
+        "'class='input'></div><div class='cell'><input type='text' maxlength='1' id='" + (count + 9) + "'class='input'></div>"
 }
 
 var level;
